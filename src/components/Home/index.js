1db5c9abba23d2cd53185941c501e384d6a12acf
yongseok <iyongseok@iyongseog-ui-MacBookPro.local>
2020-11-03 21:55:03 +0900

    Add: skills

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,12 +3,6 @@ import './Home.scss';
 
 const Home = () => {
   return (
-    // <div className="bg">
-    //   <div className="intro">
-    //     <div>Hello, I'm Yong seok;</div>
-    //     <div>Java Script Developer;</div>
-    //   </div>
-    // </div>
     <div className="home">
       <div className="home-container">
         <div className="title">
@@ -17,25 +11,7 @@ const Home = () => {
           <div>JavaScript Developer.</div>
         </div>
       </div>
-      {/* <Container className="home-container">
-        <Row>
-          <Col xs={12} sm={12} md={12}>
-            <div className="title">
-              <div>Hello🖐,</div>
-              <div>I’m YONG SEOK.</div>
-              <div>JavaScript Developer.</div>
-            </div>
-          </Col>
-        </Row>
-      </Container> */}
     </div>
-    // <div class="bg">
-    //   <img src="/home.png" alt="" />
-    //   <div class="content">
-    //     <div>Hello, I'm Yong seok;</div>
-    //     <div>Java Script Developer;</div>
-    //   </div>
-    // </div>
   );
 };
 
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import './Skills.scss';
 
 const Skills = () => {
-  return <div>Hello, This page is Skills</div>;
+  return (
+    <div className="body">
+      <div className="body-head">
+        <div className="head-title">Skills</div>
+        <div className="title-line"></div>
+      </div>
+      <div className="skills-container">
+        <div className="skill-list">
+          <div className="skill">
+            <img src="devyong2/logo/javascript_logo.png" alt="" />
+            <div>javaScript</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/html5_logo.png" alt="" />
+            <div>Html5</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/nodejs_logo.png" alt="" />
+            <div>Node.js</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/css3_logo.png" alt="" />
+            <div>css3</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/jQuery_logo.png" alt="" />
+            <div>jQuery</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/react_logo.png" alt="" />
+            <div>React</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/angular_logo.png" alt="" />
+            <div>Angular</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/react_logo.png" alt="" />
+            <div>React Native</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/ionic_logo.png" alt="" />
+            <div>Ionic</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/typeScript_logo.png" alt="" />
+            <div>TypeScript</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/firebase_logo.png" alt="" />
+            <div>Fire Base</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/aws_logo.png" alt="" />
+            <div>AWS</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/sass_logo.png" alt="" />
+            <div>sass (scss)</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/styled-components_logo.png" alt="" />
+            <div>styled-components</div>
+          </div>
+          <div className="skill">
+            <img src="devyong2/logo/github_logo.png" alt="" />
+            <div>Git Hub</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
